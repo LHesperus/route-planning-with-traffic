b9f28f5f3649d3048e5878c9ce8966772d26b947
LHesperus <2639350497@qq.com>
2019-03-26 23:02:36 +0800

    Update CodeCraft-2019.cpp
    改了类的一些函数，正在写Dijkstra，写了一点，先保存下来

--- mycode/CodeCraft-2019/CodeCraft-2019.cpp
+++ mycode/CodeCraft-2019/CodeCraft-2019.cpp
@@ -5,6 +5,7 @@
 
 #include <math.h>
 using namespace std;
+
 //define class
 //***********************************************
 //************************************************
@@ -12,7 +13,7 @@ using namespace std;
     {
 		public:
 		void set_num(int a,int b,int c,int d,int e,int f,int g);
-		void dis_num();
+		int dis_num(int n);
         private:
         int road_id,road_length,road_speed,road_channel,road_from,road_to,road_isDuplex;  
     };
@@ -27,16 +28,35 @@ void Road::set_num(int a,int b,int c,int d,int e,int f,int g)
 			road_to=f;
 			road_isDuplex=g;
 		}
-void Road::dis_num()//测试用
+int Road::dis_num(int n)//测试用
 {
-    cout<<road_id<<endl;
-    cout<<road_length<<endl;
-    cout<<road_speed<<endl;
-    cout<<road_channel<<endl;
-    cout<<road_from<<endl;
-    cout<<road_to<<endl;
-    cout<<road_isDuplex<<endl;
-}
+//    cout<<road_id<<endl;
+//    cout<<road_length<<endl;
+//    cout<<road_speed<<endl;
+//    cout<<road_channel<<endl;
+//    cout<<road_from<<endl;
+//    cout<<road_to<<endl;
+//    cout<<road_isDuplex<<endl;
+	switch(n)
+	{
+		case 1:
+			return road_id;
+		case 2:
+			return road_length;
+		case 3:
+			return road_speed;
+		case 4:
+			return road_channel;
+		case 5:
+			return road_from;
+		case 6:
+			return road_to;
+		case 7:
+			return road_isDuplex;
+        default:
+            return -1;
+	}
+};
 //*****************************************************
 //#(id,from,to,speed,planTime)
 	class Car
@@ -71,9 +91,11 @@ void Car::dis_num()//测试用
     {
 		public:
 		void set_num(int a,int b,int c,int d,int e);
-		void dis_num();
+        void set_W_Dij(int a);
+		int dis_num(int n );
         private:
-        int cross_id,cross_roadId_1,cross_roadId_2,cross_roadId_3,cross_roadId_4;  
+        int cross_id,cross_roadId_1,cross_roadId_2,cross_roadId_3,cross_roadId_4,cross_W_Dij=10000;  //Dijkstra 算法的顶点标号大小，初始尽量大
+		int cross_roadId[4];//数组表示，与上面重复定义，有时间在修改
     };
     
 void Cross::set_num(int a,int b,int c,int d,int e)
@@ -83,17 +105,89 @@ void Cross::set_num(int a,int b,int c,int d,int e)
 			cross_roadId_2=c;
 			cross_roadId_3=d;
 			cross_roadId_4=e;
+			cross_roadId[0]=b;
+			cross_roadId[1]=c;
+			cross_roadId[2]=d;
+			cross_roadId[3]=e;
 		}
-void Cross::dis_num()//测试用
-{
-	cout<<cross_id<<endl;
-    cout<<cross_roadId_1<<endl;
-    cout<<cross_roadId_2<<endl;
-    cout<<cross_roadId_3<<endl;
-    cout<<cross_roadId_4<<endl;
-}
-//*************************************************************
+void Cross::set_W_Dij(int a){cross_W_Dij=a;}
 
+int Cross::dis_num(int n)//测试用
+{
+//	cout<<cross_id<<endl;
+//    cout<<cross_roadId_1<<endl;
+//    cout<<cross_roadId_2<<endl;
+//    cout<<cross_roadId_3<<endl;
+//    cout<<cross_roadId_4<<endl;
+	switch(n)
+	{
+		case 1:
+			return cross_id;
+		case 2:
+			return cross_roadId[0];
+		case 3:
+			return cross_roadId[1];
+		case 4:
+			return cross_roadId[2];
+		case 5:
+			return cross_roadId[3];	
+		case 6:
+			return cross_W_Dij;
+        default:
+            return -1;
+	}
+};
+//*************************************************************
+//车、路、路口数组类,参数需要修改
+static Cross Cross_group[10000];	//路口
+static Road Road_group[10000];    //路
+static Car Car_group[10000];		//车
+//Dijkstra算法找到两点最短路径，返回数组
+
+int* Common_Dijkstra(int p_start,int p_end)
+{	
+	//Cross_group[13].dis_num();	//test
+	//int a[10]={0}; //test
+	int path[100];		//最短路，为路口数据，参数需要修改
+	int cross_N=0,cross_N_next=0,road_N=0;
+	
+	for (int i=0;i<10000;i++) //参数需要修改，与路口数量匹配
+	{
+		if( Cross_group[i].dis_num(1)==p_start)	//找到对应的路口id
+		{
+			cross_N=i;
+			Cross_group[cross_N].set_W_Dij(0);
+			break;
+		}
+	}
+	for (int j=0;j<4;j++)   
+	{
+		for (int i=0;i<10000;i++)   
+		{
+			if (Road_group[i].dis_num(1)==Cross_group[cross_N].dis_num(j+2))//找到路口相连边的id
+			{	
+				for (int ii=0;ii<10000;ii++)
+				{
+					if(Road_group[i].dis_num(6)==Cross_group[ii].dis_num(1))//找到对应边终点的路口id
+					{
+						cross_N_next=ii;
+						break;
+					}
+				}
+					
+				if((Cross_group[cross_N].dis_num(6)+Road_group[i].dis_num(2))<=Cross_group[cross_N_next].dis_num(6))//判断顶点权重加上边长后 与 边终点路口权重的大小
+						Cross_group[cross_N_next].set_W_Dij(Cross_group[cross_N].dis_num(6)+Road_group[i].dis_num(2));////更新路口权重
+			}
+		
+		}
+		
+		
+	}
+	
+	
+	
+	return path;
+}
 int main(int argc, char *argv[])
 {
     cout << "Begin" << endl;
@@ -112,14 +206,14 @@ int main(int argc, char *argv[])
 	cout << "roadPath is " << roadPath << endl;
 	cout << "crossPath is " << crossPath << endl;
 	cout << "answerPath is " << answerPath << endl;
-//车、路、路口数组类
+
 //*****************************************************************
 //*****************************************************************
-Road Road_group[10000];    //路
-Car Car_group[10000];		//车
-Cross Cross_group[10000];	//路口
+
+
 int road_num=0,car_num=0,cross_num=0;
 
+	// TODO:read input filebuf
 //读文件，目前第一行注释也读进去了，有时间再处理
 ifstream fin("../config/road.txt", ios::in);
 char line[100]={0};//存储每行字符
@@ -257,24 +351,24 @@ while(fin2.getline(line, sizeof(line)))
             j++;
         switch(k)
         {
-            case 0:           
-                a=(x[i]-zero)*int(pow(10,j))+a;
-                break;
-            case 1:
-                b=(x[i]-zero)*int(pow(10,j))+b;
-                break;
-            case 2:
-                c=(x[i]-zero)*int(pow(10,j))+c;
-                break;
-            case 3:
-                d=(x[i]-zero)*int(pow(10,j))+d;
-                break;        
-            case 4:
-                e=(x[i]-zero)*int(pow(10,j))+e;
-                break;        
-            case 5:
-                f=(x[i]-zero)*int(pow(10,j))+f;
-                break;        
+          case 0:           
+              a=(x[i]-zero)*int(pow(10,j))+a;
+              break;
+          case 1:
+              b=(x[i]-zero)*int(pow(10,j))+b;
+              break;
+          case 2:
+              c=(x[i]-zero)*int(pow(10,j))+c;
+              break;
+          case 3:
+              d=(x[i]-zero)*int(pow(10,j))+d;
+              break;        
+          case 4:
+              e=(x[i]-zero)*int(pow(10,j))+e;
+              break;        
+          case 5:
+              f=(x[i]-zero)*int(pow(10,j))+f;
+              break;        
             case 6:
                 g=(x[i]-zero)*int(pow(10,j))+g;
                 break;          
@@ -286,15 +380,19 @@ a=0;b=0;c=0;d=0;e=0;
 }
 fin2.clear();
 fin2.close();
-Cross_group[4].dis_num();//测试用
+//Cross_group[4].dis_num();//测试用
 //**********************************************************
 
-
-
-
-	// TODO:read input filebuf
+//***********************************************************
 	// TODO:process
+	int* p;
+p=Common_Dijkstra(1,2);
+	cout<< p <<endl;
+	
 	// TODO:write output file
 	
 	return 0;
 }
+
+
+
